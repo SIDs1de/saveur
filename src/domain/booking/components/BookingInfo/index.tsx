2742de44290278e079b63727f35854562573a16a
Игорь Денисенков <igorekdenisenkov@yandex.ru>
2026-05-31 19:24:11 +0300

    KAN-11: feat: разработка BookingInfo

--- src/domain/booking/components/BookingInfo/index.tsx
+++ src/domain/booking/components/BookingInfo/index.tsx
@@ -1,5 +1,35 @@
+import styles from './index.module.scss';
 import { BookingInfoProps } from './types';
+import clsx from 'clsx';
 
 export const BookingInfo = ({ className }: BookingInfoProps) => {
-  return <div className={className}>123</div>;
+  return (
+    <div className={clsx(styles.root, className)}>
+      <div>
+        <span className={styles.subtitle}>ОНЛАЙН-БРОНИРОВАНИЕ</span>
+        <h1>
+          Ваш столик <span className={styles.highlight}>уже ждёт.</span>
+        </h1>
+        <p className={styles.description}>
+          Оставьте детали визита, а мы подготовим всё к вашему приходу.
+        </p>
+      </div>
+      <div>
+        <div className={styles.groups}>
+          <address className={styles.group}>
+            <span className={styles.groupLabel}>АДРЕС</span>
+            <span className={styles.groupValue}>Малая Бронная, 14</span>
+          </address>
+          <div className={styles.group}>
+            <span className={styles.groupLabel}>ЧАСЫ РАБОТЫ</span>
+            <span className={styles.groupValue}>Ежедневно, 12:00–23:00</span>
+          </div>
+        </div>
+        <p className={styles.additionalInfo}>
+          Для компаний больше 12 гостей позвоните нам:{' '}
+          <a href="tel:+74951234567">+7 (495) 123-45-67</a>
+        </p>
+      </div>
+    </div>
+  );
 };
--- src/shared/config/fonts.ts
+++ src/shared/config/fonts.ts
@@ -3,9 +3,12 @@ import { Cormorant_Garamond, Inter } from 'next/font/google';
 export const inter = Inter({
   subsets: ['latin', 'cyrillic'],
   variable: '--font-inter',
+  weight: ['400', '700', '800'],
 });
 
 export const cormorantGaramond = Cormorant_Garamond({
   subsets: ['latin', 'cyrillic'],
+  style: ['normal', 'italic'],
   variable: '--font-cormorant-garamond',
+  weight: ['600'],
 });
